d27e04370e5fa808b46eb621fa9d82b0290d79f8
LLS-lopos <luderickls@gmail.com>
2026-05-14 10:40:53 -0400

    clean code

--- io_supertuxkart_engine/moteur/proprieter.py
+++
@@ -1,5 +0,0 @@
-enum_calcul = (
-    ('CPU', "CPU", "Use CPU for rendering"),
-    ('GPU', "GPU Compute",
-     "Use GPU compute device for rendering, configured in the system tab in the user preferences"),
-)
