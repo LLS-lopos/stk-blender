fc1206e385ff2f6b4dd0bc394605d415d078fbb4
LLS-lopos <luderickls@gmail.com>
2026-09-02 13:49:32 -0400

    local

--- io_antarctica_scene/stk_track.py
+++ io_antarctica_scene/stk_track.py
@@ -1275,8 +1275,8 @@ class STK_Track_Export_Operator(bpy.types.Operator):
             return False
 
     def invoke(self, context, event):
-        isATrack = context.scene['is_stk_track'] == 'true'
-        isANode = context.scene['is_stk_node'] == 'true'
+        isATrack = ('is_stk_track' in context.scene) and (context.scene['is_stk_track'] == 'true')
+        isANode = ('is_stk_node' in context.scene) and (context.scene['is_stk_node'] == 'true')
 
         # FIXME: in library nodes it's "name", in tracks it's "code"
         if isANode:
@@ -1318,8 +1318,8 @@ class STK_Track_Export_Operator(bpy.types.Operator):
             # Return to object mode before exporting
             bpy.ops.object.mode_set(mode='OBJECT')
 
-        isNotATrack = context.scene['is_stk_track'] != 'true'
-        isNotANode = context.scene['is_stk_node'] != 'true'
+        isNotATrack = ('is_stk_track' not in context.scene) or (context.scene['is_stk_track'] != 'true')
+        isNotANode = ('is_stk_node' not in context.scene) or (context.scene['is_stk_node'] != 'true')
 
         if self.filepath == "" or (isNotATrack and isNotANode):
             return {'FINISHED'}
