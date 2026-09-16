5e66eb5b69da93a2ef887bf60720eeb83d9d841d
LLS-lopos <luderickls@gmail.com>
2026-09-16 14:24:55 -0400

    add a base node in geo-node for replace particles system of blender

--- io_artiste/__init__.py
+++ io_artiste/__init__.py
@@ -12,6 +12,7 @@ import bpy
 from .base import (node_base, menu, node_editor)
 from .node.init import (cli, init_stk)
 from .node.run import (runner)
+from .geo_node import (stk_geo_node)
 
 classes = (
     node_editor.STKeditor,
@@ -26,6 +27,10 @@ classes = (
     cli.STK_cli,
     runner.STK_run,
     runner.STK_OT_RunStk,
+    stk_geo_node.STKNodeMenu,
+    stk_geo_node.STKparticles,
+    stk_geo_node.ListePoint,
+    stk_geo_node.CheckActif,
 )
 
 def add_stk_node_menu(self, context):
@@ -33,16 +38,22 @@ def add_stk_node_menu(self, context):
     self.layout.menu(menu.STKoperator.bl_idname)
     self.layout.menu(menu.STKmode.bl_idname)
     self.layout.menu(menu.STKdebug.bl_idname)
-        
+
+# Define custom STK geometry node
+def menu_stk_geo_node(self, context):
+    self.layout.menu(stk_geo_node.STKNodeMenu.bl_idname, text="SuperTuxKart", icon='AUTO')
+
 
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
     bpy.types.NODE_MT_add.append(add_stk_node_menu)
     bpy.app.handlers.depsgraph_update_post.append(node_editor.STKeditor.update_scene_handler)
+    bpy.types.NODE_MT_geometry_node_add_all.append(menu_stk_geo_node)
 
 def unregister():
     bpy.types.NODE_MT_add.remove(add_stk_node_menu)
+    bpy.types.NODE_MT_geometry_node_add_all.remove(menu_stk_geo_node)
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
     bpy.app.handlers.depsgraph_update_post.remove(node_editor.STKeditor.update_scene_handler)
